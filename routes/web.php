ea6bed4cb6a338123607e33ab01e5fc726f169b8
mualiyu <mualiyuoox@gmail.com>
2023-04-05 08:56:25 +0100

    Paho MQTT with vanila JS is added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActuatorController;
+use App\Http\Controllers\SensorDetailController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
